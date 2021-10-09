c4ca0dbb115b95ca49274ddc3a211d52b157b70c
amirrezaask <raskarpour@gmail.com>
2021-10-09 23:56:46 +0330

    Adds a simple tcp server that accepts connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, io};
+
+use tokio::net::TcpListener;
+
+#[tokio::main]
+pub async fn main() -> Result<(), io::Error> {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:6142".to_string());
+    let listener = TcpListener::bind(&addr).await?;
+
+    loop {
+        // Asynchronously wait for an inbound TcpStream.
+        let (stream, addr) = listener.accept().await?;
+    }
 }
